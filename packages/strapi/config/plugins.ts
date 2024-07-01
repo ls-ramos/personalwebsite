3646adedd04f14ade5ce63ebe702a947274ab830
Lucas Ramos <lucas.sramos.de@gmail.com>
2024-07-01 20:13:17 +0200

    Use external bucket for media

--- packages/strapi/config/plugins.ts
+++ packages/strapi/config/plugins.ts
@@ -1,1 +1,13 @@
-export default () => ({});
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: "strapi-provider-firebase-storage",
+      providerOptions: {
+        serviceAccount: JSON.parse(env("SA_BUCKET", "{}")),
+        bucket: env("STORAGE_BUCKET_URL"),
+        sortInStorage: true,
+        debug: false, 
+      },
+    },
+  },
+});
